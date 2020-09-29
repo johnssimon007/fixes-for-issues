8d664eeedcf04e392a13b0bd2942eb88bedc7d66
johnssimon007 <johnssimon007@gmail.com>
2020-09-29 14:45:21 +0530

    added open redirect fix

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,16 @@ if (location.hostname === host) {
     console.log = function () {};
 }
 
-//replace malicious chars in url
+
 function redirect() {
-    window.location.href=location.href.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/javascript:/g,'');
+      //check for untrusted domain
+      //replace 127.0.0.1 with your domain name
+      if(!location.href.match(/^http:\/\/(www\.)?127.0.0.1/i)){
+        location.href="http://127.0.0.1" //if regex doesnt match browser wil redirect to default location
+      }
+
+      //replace malicious chars in url
+      window.location.href=location.href.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/javascript:/g,'');
+
 }
 redirect()
